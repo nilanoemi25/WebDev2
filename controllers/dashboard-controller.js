4498c2ff233bbcbf0b4038fc15f3fe9100c27dc5
Noemi Lovei <nila.noemi25@gmail.com>
2024-06-22 12:39:55 +0100

    Added User on to WeatherTop

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,10 +1,12 @@
 import { stationStore } from "../models/station-store.js";
+import { accountsController } from "./accounts-controller.js"; 
 
 export const dashboardController = {
   async index(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
     const viewData = {
       title: "WeatherTop Dashboard",
-      station: await stationStore.getAllStations(),
+      station: await stationStore.getStationsByUserId(loggedInUser._id),
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
@@ -12,8 +14,10 @@ export const dashboardController = {
   },
   
   async addStation(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
     const newStation = {
       title: request.body.title,
+      userid: loggedInUser._id,
     };
     console.log(`adding station ${newStation.title}`);
     await stationStore.addStation(newStation);
--- routes.js
+++ routes.js
@@ -3,10 +3,17 @@ import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
 import {stationController} from "./controllers/station-controller.js";
 import { ReportController } from "./controllers/report-controller.js";
+import { accountsController } from './controllers/accounts-controller.js';
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 router.post("/dashboard/addlocation", dashboardController.addStation);
