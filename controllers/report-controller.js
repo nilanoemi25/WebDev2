76df646122dbcbf659debcf254b1590b742c350b
Noemi Lovei <nila.noemi25@gmail.com>
2024-06-24 19:25:51 +0100

    Release 3 Added Time Stamp

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -4,12 +4,14 @@ import { reportStore } from "../models/report-store.js";
 export const ReportController = {
 async addReport(request, response) {
     const station = await stationStore.getStationById(request.params.id);
+    const date = new Date().toISOString();
     const newReport = {
       title: request.body.title,
+      date: date, 
       code: Number(request.body.code),
       temperature: Number(request.body.temperature),
       windspeed: Number(request.body.windspeed),
-      winddirection: Number(request.body.winddirection),
+      winddirection: request.body.winddirection, 
       pressure: Number(request.body.pressure),   
     };
     console.log(`adding report ${newReport.code}`);
